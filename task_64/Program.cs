04bd2ad9799b7b1dcf04506ce7149b4f2b23d13a
Ponikarovskiy <ponikarovsky.a@gmail.com>
2023-03-03 15:43:50 +0300

    Решение задачи № 64

--- task_64/Program.cs
+++ task_64/Program.cs
@@ -2,3 +2,26 @@
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 
+Console.Clear();
+System.Console.WriteLine();
+System.Console.WriteLine("*Задача № 64. Вывести натуральные числа в промежутке от N до 1*");
+System.Console.WriteLine();
+System.Console.WriteLine("Введите число n");
+if (!int.TryParse(Console.ReadLine(), out int n) || n < 1)
+{
+    System.Console.WriteLine("Ошибка ввода");
+    return;
+}
+
+void PrintNumber(int n)
+{
+    if (n == 1) Console.Write($"{n} ");
+    else
+    {
+        System.Console.Write($"{n} ");
+        PrintNumber(n - 1);
+    }
+}
+
+PrintNumber(n);
+System.Console.WriteLine();
